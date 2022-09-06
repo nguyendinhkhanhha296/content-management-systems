59d29591738d441b4d06d37de213736d272d24ec
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-09-06 03:21:29 -0700

    make menu of choices

--- server.js
+++ server.js
@@ -1,11 +1,94 @@
-const express = require('express');
 const sequelize = require('./config/connection');
-const app = express();
-const PORT = process.env.PORT || 3001;
+const inquirer = require('inquirer');
+require("console.table");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+// connect to the mysql server and sql database
+sequelize.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    promptUser();
 });
+
+// Prompt User for Choices
+const promptUser = () => {
+    inquirer.prompt([{
+            name: 'choices',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'View All Roles',
+                'View All Departments',
+                'View All Employees By Department',
+                'View Department Budgets',
+                'Update Employee Role',
+                'Update Employee Manager',
+                'Add Employee',
+                'Add Role',
+                'Add Department',
+                'Remove Employee',
+                'Remove Role',
+                'Remove Department',
+                'Quit'
+            ]
+        }])
+        .then((answers) => {
+            const { choices } = answers;
+
+            if (choices === 'View All Employees') {
+                viewAllEmployees();
+            }
+
+            if (choices === 'View All Departments') {
+                viewAllDepartments();
+            }
+
+            if (choices === 'View All Employees By Department') {
+                viewEmployeesByDepartment();
+            }
+
+            if (choices === 'Add Employee') {
+                addEmployee();
+            }
+
+            if (choices === 'Remove Employee') {
+                removeEmployee();
+            }
+
+            if (choices === 'Update Employee Role') {
+                updateEmployeeRole();
+            }
+
+            if (choices === 'Update Employee Manager') {
+                updateEmployeeManager();
+            }
+
+            if (choices === 'View All Roles') {
+                viewAllRoles();
+            }
+
+            if (choices === 'Add Role') {
+                addRole();
+            }
+
+            if (choices === 'Remove Role') {
+                removeRole();
+            }
+
+            if (choices === 'Add Department') {
+                addDepartment();
+            }
+
+            if (choices === 'View Department Budgets') {
+                viewDepartmentBudget();
+            }
+
+            if (choices === 'Remove Department') {
+                removeDepartment();
+            }
+
+            if (choices === 'Quit') {
+                sequelize.end();
+            }
+        });
+};
